8c2b5d656b77d8436be3e9bb1b869409fc121e4f
Vini Santana <vini@administrators-MacBook-Pro.local>
2016-12-22 20:14:45 -0800

    3.9 Nested components 1

---
+++ react-3-9-nested-components-1/public/app.jsx
@@ -0,0 +1,75 @@
+var GreeterMessage = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <h1>Some H1</h1>
+          <p>Some paragraph</p>
+        </div>
+      );
+    }
+});
+
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  getInitialState: function () {
+    return {
+        name: this.props.name
+    };
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <GreeterMessage/>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+
+        <GreeterForm/>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Andrew';
+
+ReactDOM.render(
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
