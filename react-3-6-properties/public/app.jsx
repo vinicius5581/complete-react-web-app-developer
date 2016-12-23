7104a49e7515bf8434fcb99f0a955692f69a4840
Vini Santana <vini@administrators-MacBook-Pro.local>
2016-12-22 19:37:21 -0800

    Adding node_modules to .gitignore

---
+++ react-3-6-properties/public/app.jsx
@@ -0,0 +1,26 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Andrew';
+
+ReactDOM.render(
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
