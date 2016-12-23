88539ad8d2d45fd01b18f857b0055ca02c05cc02
Vini Santana <vini@administrators-MacBook-Pro.local>
2016-12-22 20:09:07 -0800

    3.7 Events and callbacks

---
+++ react-3-7-events-and-callbacks/public/app.jsx
@@ -0,0 +1,38 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
+  render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Andrew';
+
+ReactDOM.render(
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
