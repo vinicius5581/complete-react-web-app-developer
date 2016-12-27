cac2aa679959f70dcf2ddcc5a73fc7657739702d
Vini Santana <vini@administrators-MacBook-Pro.local>
2016-12-27 08:15:17 -0800

    7.3 Style nav updated 1

---
+++ react-7-3-style-nav-updated-1/app/components/Examples.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Examples = (props) => {
+  return (
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+  )
+};
+
+module.exports = Examples;
