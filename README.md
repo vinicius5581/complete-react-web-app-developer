0afb0ad55552bd7ec0b442c265d33e3d355635a8
Vini Santana <vini@administrators-MacBook-Pro.local>
2017-01-03 13:11:03 -0800

    Adding the basics to README and playing with basic props

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
 # complete-react-web-app-developer
 
+## Start package.json
+
+npm init
+
+## Express
+
+npm install express@4 --save
+
 ## Webpack
 
 sudo npm install -g webpack
 
 ### Babel
 
+babeljs.io/repl
+
 ## Devtools
 
 
 $ npm install karma@0.13.22 karma-chrome-launcher@0.2.2 karma-mocha@0.2.2 karma-mocha-reporter@2.0.0 karma-sourcemap-loader@0.3.7 karma-webpack@1.7.0 mocha@2.4.5 expect@1.14.0 --save-dev
+
+## Common functions
+
+```
+ReactDom.render(
+	<h1>Hello React!</h1>,
+	document.getElementById('app')
+);
+```
+
+```
+var Greeter = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h1>Hello React!</h1>
+			</div>
+		);
+	}
+});
+
+ReactDom.render(
+	<Greeter/>,
+	document.getElementById('app')
+);
+
+```
--- react-3-6-properties/public/app.jsx
+++ react-3-6-properties/public/app.jsx
@@ -2,26 +2,32 @@ var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
-      message: 'This is the default message!'
+      message: 'This is the default message!',
+      another: 'another'
     };
   },
   render: function () {
+    var title = this.props.title;
     var name = this.props.name;
     var message = this.props.message;
+    var another = this.props.another;
 
     return (
       <div>
-        <h1>Hello {name}!</h1>
+        <h1>{title}</h1>
+        <h2>Hello {name}!</h2>
         <p>{message + '!!'}</p>
+        <p>{another + '!! '  + this.props.message}</p>
       </div>
     );
   }
 });
 
-var firstName = 'Andrew';
+var firstName = 'Vini';
 
 ReactDOM.render(
   <Greeter 
+    title='Title' 
     name={firstName} 
     message={'sup'}
   />,
